4c009a2d2942ef142400c440110a036b07f3a9e8
DN_Physics <eduhub071302@gmail.com>
2026-03-24 15:46:01 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "dn-physics-v1";
+
+const FILES_TO_CACHE = [
+  "/DN_Physics/",
+  "/DN_Physics/index.html",
+  "/DN_Physics/style.css",
+  "/DN_Physics/script.js",
+  "/DN_Physics/manifest.json",
+  "/DN_Physics/icon-192.png",
+  "/DN_Physics/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
