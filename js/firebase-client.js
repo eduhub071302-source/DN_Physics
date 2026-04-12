bd70faf1d96c0f4d128ea477f934dc9f587e7389
DN_Physics <eduhub071302@gmail.com>
2026-04-12 16:21:50 +0530

    Update firebase-client.js

--- js/firebase-client.js
+++ js/firebase-client.js
@@ -1,6 +1,6 @@
 // firebase-client.js
 
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
@@ -9,9 +9,14 @@ const firebaseConfig = {
   authDomain: "dn-physics.firebaseapp.com",
   databaseURL: "https://dn-physics-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "dn-physics",
+  storageBucket: "dn-physics.firebasestorage.app",
+  messagingSenderId: "550016753421",
+  appId: "1:550016753421:web:8c68d2471cb87192809a44",
+  measurementId: "G-JB216F8CQ9",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export default app;
