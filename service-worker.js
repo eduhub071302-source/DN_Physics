d545a0bbd4ed1ffd9d5e02e2a8035c34293a7f65
DN_Physics <eduhub071302@gmail.com>
2026-04-12 17:57:13 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dn-physics-v260"; // 🔥 increase version
+const CACHE_NAME = "dn-physics-v261"; // 🔥 increase version
 const META_CACHE = "dn-physics-meta";
 
 const CORE_FILES = [
